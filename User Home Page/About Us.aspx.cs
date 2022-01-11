f5871a79b2a5a1e6d3db899b73bfd95243705f0a
MaryamZafar036 <96041935+MaryamZafar036@users.noreply.github.com>
2022-01-11 10:57:44 +0500

    Add files via upload

---
+++ User Home Page/About Us.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Personality_Prediction.User_Home_Page
+{
+    public partial class About_Us : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
